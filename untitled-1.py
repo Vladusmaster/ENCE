4653f24bc704d657349386c431a2c08c439839ed
Владислав <vladislavagafosin17580@gmail.com>
2022-01-12 11:34:29 +0300

    d ready

--- untitled-1.py
+++ untitled-1.py
@@ -1,8 +1,13 @@
-def D():
- print("  __   ")
- print(" |  \  ")
- print(" |   | ")
- print(" |   | ")
- print(" |   | ")
- print(" |__/  ")
-D()
+def D(buf):
+    buf[0] = buf[0] + " ╓────╖      "
+    buf[1] = buf[1] + " ╙╖█╓╖║      "
+    buf[2] = buf[2] + " ░║█║║║      "
+    buf[3] = buf[3] + " ░║█║║║      "
+    buf[4] = buf[4] + " ╓╜█╙╜║      "
+    buf[5] = buf[5] + " ╙────╜      "
+ 
+ 
+s = ["","","","","",""]
+D(s)
+D(s)
+print(*s, sep='\n')
